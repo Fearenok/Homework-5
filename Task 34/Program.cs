575f8f90aa858a2df83b86ec897b870e865052c9
Anton <polyncev1991@mail.ru>
2022-08-25 17:55:01 +0300

    Modification task 34

--- Task 34/Program.cs
+++ Task 34/Program.cs
@@ -6,23 +6,28 @@ Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
 int count = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
 
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+    for (int z = 0; z < numbers.Length; z++)
+    {
+        if (numbers[z] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
 
 void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        numbers[i] = new Random().Next(100, 1000);
     }
 }
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
         Console.Write(numbers[i] + " ");
     }
